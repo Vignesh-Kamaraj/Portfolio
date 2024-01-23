9429361fd3a5bf7b99d9c359e300f4731015cbc4
Vignesh-Kamaraj <vigneshkamaraj25@gmail.com>
2024-01-23 12:15:26 +0530

    updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,7 +9,7 @@ function Intro() {
             <span className='hello'>Hello,</span>
             <span className="introtext">I'm<span className="introname">Vignesh</span><br />Full Stack Developer</span>
             <p className="intropara">I am a skilled Full Stack Developer</p>
-            <Link to='/Contact'><button className="btn">Hire me</button></Link><Link to='https://drive.google.com/file/d/10YZtL0uAu53lw4OgyVRoNmeqf1Iwu8To/view?usp=sharing' target='_blank'><button className="btn">Resume</button></Link>
+            <Link to='/Contact'><button className="btn">Hire me</button></Link><Link to='https://drive.google.com/file/d/1INrIwEGmgcjcUBbZ5Vp3Nca5VxXRBu1p/view?usp=sharing' target='_blank'><button className="btn">Resume</button></Link>
         </div>
         <img src={my} alt="" className="bg" />
     </section>
