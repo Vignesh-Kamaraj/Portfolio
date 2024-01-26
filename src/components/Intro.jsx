83c6bb22bf04c8a3826523fb28907f25a26b1dca
Vignesh-Kamaraj <vigneshkamaraj25@gmail.com>
2024-01-26 13:31:16 +0530

    updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link} from 'react-router-dom'
 import my from '../assets/my photo (2).png'
+import na from '../assets/fy5Z.gif'
 
 function Intro() {
   return <>
     <section id="intro">
         <div className="introcontent">
             <span className='hello'>Hello,</span>
-            <span className="introtext">I'm<span className="introname">Vignesh</span><br />Full Stack Developer</span>
+            <span className="introtext">I'm<span className="introname" style={{backgroundImage:`url(${na})`}}>Vignesh</span><br />Full Stack Developer</span>
             <p className="intropara">I am a skilled Full Stack Developer</p>
             <Link to='/Contact'><button className="btn">Hire me</button></Link><Link to='https://drive.google.com/file/d/1INrIwEGmgcjcUBbZ5Vp3Nca5VxXRBu1p/view?usp=sharing' target='_blank'><button className="btn">Resume</button></Link>
         </div>
